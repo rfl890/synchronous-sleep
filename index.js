149687266b16430ec3aa7f6496300fcec3d67f3f
rfl890 <87506407+rfl890@users.noreply.github.com>
2021-11-11 12:14:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
+const chalk = require("chalk");
+let warnsEnabled = true;
+function disableWarns() {
+    warnsEnabled = false;
+}
 function sleepms(t) {
-    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, t);
+    let ttw = t;
+    if (!ttw) {
+        throw new Error("Please specify a number to sleep.");
+    }
+    if (!(typeof (ttw) === "number")) {
+        throw new Error("Please provide an number to sleep, not a bool, string etc...");
+    }
+    if (ttw < 5) {
+        if (warnsEnabled) {
+            console.warn(chalk.yellow("[WARN]: (synchronous-sleep) You cannot sleep under 5 ms. Setting to 5 ms.\nTo disable warns, run the disableWarns() function at the start of your code."));
+        }
+        ttw = 5;
+    }
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ttw);
 }
 function sleep(t) {
-    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, t*1000);
+    let ttw = t;
+    if (!ttw) {
+        throw new Error("Please specify a number to sleep.");
+    }
+    if (!(typeof (ttw) === "number")) {
+        throw new Error("Please ");
+    }
+    if (ttw < 0.05) {
+        if (warnsEnabled) {
+            console.warn(chalk.yellow("[WARN]: (synchronous-sleep) You cannot sleep under 5 ms or 0.05 s. Setting to 5 ms.\nTo disable warns, run the disableWarns() function at the start of your code."));
+        }
+        ttw = 0.05;
+    }
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, t * 1000);
 }
 module.exports = {
     sleepms,
-    sleep
+    sleep,
+    disableWarns
 }
